1eeecc8bcfc7f8a84dae03942cee1f9efcbd51dd
zayneio <hello@zayne.io>
2018-12-23 21:55:39 -0800

    add base url to search module

--- lib/twumper/search.rb
+++ lib/twumper/search.rb
@@ -1,6 +1,7 @@
 module Twumper
   module Search
     
+    BASE_URL = "https://api.twitter.com/1.1/search/tweets.json"
     DEFAULT_OPTIONS = { 
       count: 100, 
       limit: 200, 
@@ -11,7 +12,7 @@ module Twumper
     def search(keyword, options=DEFAULT_OPTIONS) 
       tweets = Array.new
       while tweets.count < options[:limit]
-        url = "https://api.twitter.com/1.1/search/tweets.json?q=#{keyword}&result_type=#{options[:result_type]}&count=#{options[:count]}"
+        url = "#{BASE_URL}?q=#{keyword}&result_type=#{options[:result_type]}&count=#{options[:count]}"
         url += "&max_id=#{options[:max_id]}" if !options[:max_id].nil?
         headers = set_headers
         response = connection.get(url, headers: headers)
@@ -19,6 +20,6 @@ module Twumper
         options[:max_id] = tweets.last['id']
       end
       tweets
-    end      
+    end
   end
 end
