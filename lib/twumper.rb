f1155f00a982eb0d8f52928bb3e5cc39adba6bf9
zayneio <hello@zayne.io>
2018-12-23 20:25:08 -0800

    update license; remove version from main

--- lib/twumper.rb
+++ lib/twumper.rb
@@ -8,11 +8,6 @@ module Twumper
       @client ||= Twumper::Client.new
     end
 
-    def method_missing(method, *args, &block)
-      return super unless new.respond_to?(method)
-      new.send(method, *args, &block)
-    end
-
     def respond_to?(method, include_private = false)
       new.respond_to?(method, include_private) || super
     end
--- lib/twumper/config.rb
+++ lib/twumper/config.rb
@@ -1,7 +1,6 @@
-require 'byebug'
-
 module Twumper
   module Config
+    include Auth
     
     attr_accessor :headers, :consumer_key, :consumer_secret, :bearer
 
@@ -28,7 +27,7 @@ module Twumper
       headers = set_headers(credentials)
       request_bearer(url, body, headers)
     end
-    
+
     def set_credentials
       credentials = Base64.encode64("#{self.consumer_key}:#{self.consumer_secret}").gsub("\n", '')
     end
@@ -44,19 +43,6 @@ module Twumper
     def request_bearer(url, body, headers)
       resp = connection.post(url, body: body, headers: headers)
       bearer_token = resp['access_token']
-    end
-
-    # def search(keyword, options={count: 100, limit: 200, result_type: 'mixed', max_id: nil}) 
-    #   tweets = Array.new
-    #   while tweets.count < options[:limit]
-    #     url = "https://api.twitter.com/1.1/search/tweets.json?q=#{keyword}&result_type=#{options[:result_type]}&count=#{options[:count]}"
-    #     url += "&max_id=#{options[:max_id]}" if !options[:max_id].nil?
-    #     headers = set_headers
-    #     response = connection.get(url, headers: headers)
-    #     tweets += response['statuses']
-    #     options[:max_id] = tweets.last['id']
-    #   end
-    #   tweets
-    # end        
+    end    
   end
 end
